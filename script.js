d7177b4a996856b7f502847a95e4ccf28dd7d0b0
Mohammad Yusuf Khan <98419143+m-y-k@users.noreply.github.com>
2023-06-25 06:05:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function promiseMaker() {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve("Hello, world!")
+		}, 1000);
+	});
+}
+
+let x = promiseMaker();
+x.then((data) => {
+	const ele = document.getElementById("output");
+	ele.innerText = data;
+});
